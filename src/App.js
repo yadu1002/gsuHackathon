01d45769c170bcf9f2d4af8e81d83a1d0e427659
yadu1002 <83854011+yadu1002@users.noreply.github.com>
2022-03-19 20:36:34 -0400

    added the local files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to PantherPark.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Link to react page
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
